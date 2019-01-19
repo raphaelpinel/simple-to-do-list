da48d845af80a5e511f19f7e5f49744a62f9996c
Raphael Pinel <raphaelpinel8@gmail.com>
2019-01-19 10:37:41 +0200

    structured application into components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './components/Header/Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          To-Do List <i className="fas fa-plus" />
-        </h1>
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <h1>
+    TO-DO LIST
+    <i className="fas fa-plus" />
+  </h1>
+);
+
+export default Header;
